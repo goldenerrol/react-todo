e2d2961cc8b9f07e1c8d39e788856dff59f3cb3d
Dan Goldberg <d.goldberg82@gmail.com>
2016-09-11 17:03:30 -0400

    Adding todoApp.test toggle functions

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -10,7 +10,7 @@ var TodoList = React.createClass({
           // ... is the Spread operator, lets you spread out all the properties of
           // an object. In this case, the props of each Todo item! being passed to Todo.jsx
           //Todo Key tells which value is the key value (unique ID)
-          <Todo key={todo.id} {...todo}/>
+          <Todo key={todo.id} {...todo} onToggle={this.props.onToggle}/>
         );
       });
     };
