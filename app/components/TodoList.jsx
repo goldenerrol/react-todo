dd59011ede0c77c691abc3adef24ef31aaec685b
Dan Goldberg <d.goldberg82@gmail.com>
2016-09-14 16:42:47 -0400

    Style Todo Application

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -5,6 +5,11 @@ var TodoList = React.createClass({
   render: function () {
     var {todos} = this.props;
     var renderTodos = () => {
+      if (todos.length === 0) {
+        return (
+          <p className="container__message">Nothing To Do</p>
+        );
+      }
       return todos.map((todo) => {
         return (
           // ... is the Spread operator, lets you spread out all the properties of
